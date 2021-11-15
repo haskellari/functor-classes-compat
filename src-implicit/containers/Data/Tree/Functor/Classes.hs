62f377d079c38f460bc7d0f8f22ba4d6855ea5d8
Oleg Grenrus <oleg.grenrus@iki.fi>
2021-11-15 17:07:33 +0200

    Issue #23

--- src-implicit/containers/Data/Tree/Functor/Classes.hs
+++ src-implicit/containers/Data/Tree/Functor/Classes.hs
@@ -2,18 +2,20 @@
 module Data.Tree.Functor.Classes () where
 
 import Data.Functor.Classes
-import Data.Tree            (Tree(..))
+import Data.Tree            (Tree (..))
+
+import qualified Data.Monoid as M
 
 instance Eq1 Tree where eq1 = (==)
 instance Ord1 Tree where
   compare1 (Node a fa) (Node b fb) =
-    compare a b `mappend` liftCompareList compare1 fa fb
+    compare a b `M.mappend` liftCompareList compare1 fa fb
 
 liftCompareList :: (a -> a -> Ordering) -> [a] -> [a] -> Ordering
 liftCompareList cmp = go
   where go [] [] = EQ
         go [] (_:_) = LT
         go (_:_) [] = GT
-        go (a:as) (b:bs) = cmp a b `mappend` go as bs
+        go (a:as) (b:bs) = cmp a b `M.mappend` go as bs
 
 instance Show1 Tree where showsPrec1 = showsPrec
